f4c9cfa1d801083f055982b09e1e8433c31c7a19
Kaveri Kamble <kaveristpt@gmail.com>
2025-07-04 11:48:34 +0100

    First part

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class FairSplit
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to FairSplit!");
+
+        // Get total amount
+        Console.Write("Enter the total amount (£): ");
+        decimal totalAmount = Convert.ToDecimal(Console.ReadLine());
+
+        // Get number of people
+        Console.Write("Enter the number of people to split the bill: ");
+        int numberOfPeople = Convert.ToInt32(Console.ReadLine());
+
+        // Calculate and display the share
+        if (numberOfPeople <= 0)
+        {
+            Console.WriteLine("Number of people must be greater than zero.");
+        }
+        else
+        {
+            decimal share = totalAmount / numberOfPeople;
+            Console.WriteLine($"\nEach person should pay: £{share:F2}");
+        }
+
+        Console.WriteLine("\nThanks for using FairSplit!");
+    }
+}
+// **Features to add:**
+//- Input validation to prevent dividing by zero
+// Simple and clean calculation
+//- Formatted output for currency
+
+// expand it with names, rounding options
+
--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+beb3bf70372527f0cdb9be6dd2831d1dc7983326")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
